c293306744fd5841b25427681abdb2abf138de76
ZARETHz <zarethsan13@gmail.com>
2026-03-19 09:18:55 -0600

    five

---
+++ 19-03-26/ej5.py
@@ -0,0 +1,17 @@
+numeros = [5, 10, 25, 20, 25]
+
+def promedio(numeros):
+    return sum(numeros) / len(numeros)
+
+def maximo(numeros):
+    return max(numeros)
+
+def minimo(numeros):
+    return min(numeros)
+
+
+numeros = [5, 10, 25, 20, 25]
+
+print(promedio(numeros))
+print(maximo(numeros))
+print(minimo(numeros))
